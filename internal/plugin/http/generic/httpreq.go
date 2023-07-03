0611f6f76afab1c1415d4cb985240793fbc8e7ba
555f <555f@gmail.com>
2023-07-03 10:17:44 +0300

    fix: :bug: htt request gen added time.Time type

--- internal/plugin/http/generic/httpreq.go
+++ internal/plugin/http/generic/httpreq.go
@@ -20,19 +20,26 @@ func jsonFromType(name string, t any) string {
 
 	switch v := t.(type) {
 	case *types.Named:
-		if st := v.Struct(); st != nil {
-			buf.WriteString("{\n")
-			for _, f := range v.Struct().Fields {
-				name := f.Var.Name
-				if t, err := f.SysTags.Get("json"); err == nil {
-					name = t.Value()
+		name := v.Pkg.Path + "." + v.Name
+		switch name {
+		default:
+			if st := v.Struct(); st != nil {
+				buf.WriteString("{\n")
+				for _, f := range v.Struct().Fields {
+					name := f.Var.Name
+					if t, err := f.SysTags.Get("json"); err == nil {
+						name = t.Value()
+					}
+					buf.WriteString("  " + jsonFromType(name, f.Var.Type))
 				}
-				buf.WriteString("  " + jsonFromType(name, f.Var.Type))
+				buf.WriteString("  }")
+			} else {
+				buf.WriteString("\"\"")
 			}
-			buf.WriteString("  }")
-		} else {
+		case "time.Time":
 			buf.WriteString("\"\"")
 		}
+
 	case *types.Slice, *types.Array:
 		buf.WriteString("[]")
 	case *types.Basic:
