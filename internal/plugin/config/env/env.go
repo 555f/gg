eef1e85855447d9c864697e649008a14ebd9b98a
555f <555f@gmail.com>
2023-10-04 16:13:13 +0300

    feat: added use multierror pkg for config gen

--- internal/plugin/config/env/env.go
+++ internal/plugin/config/env/env.go
@@ -8,10 +8,12 @@ import (
 	. "github.com/dave/jennifer/jen"
 )
 
+const multierrorPkg = "github.com/hashicorp/go-multierror"
+
 func GenConfig(c options.Config) func(f *file.GoFile) {
 	return func(f *file.GoFile) {
 		stType := f.Import(c.PkgPath, c.Name)
-		f.Func().Id(c.ConstructName).Params().Params(Id("c").Op("*").Do(stType), Id("errs").Index().Error()).BlockFunc(func(g *Group) {
+		f.Func().Id(c.ConstructName).Params().Params(Id("c").Op("*").Do(stType), Id("errs").Error()).BlockFunc(func(g *Group) {
 			g.Id("c").Op("=").Op("&").Do(stType).Values()
 
 			walkFields(c.Fields, func(parent *options.ConfigField, field options.ConfigField) {
@@ -39,21 +41,21 @@ func GenConfig(c options.Config) func(f *file.GoFile) {
 						} else {
 							g.Add(gen.ParseValue(Id("s"), Id("v"), ":=", field.Type, f.Import))
 							g.Do(gen.CheckErr(
-								Id("errs").Op("=").Append(Id("errs"), Qual("fmt", "Errorf").Call(Lit("env "+envName+" failed parse: %w"), Err())),
+								Id("errs").Op("=").Qual(multierrorPkg, "Append").Call(Id("errs"), Qual("fmt", "Errorf").Call(Lit("env "+envName+" failed parse: %w"), Err())),
 							))
 							g.Add(code).Dot(field.FieldName).Op("=").Id("v")
 						}
 						if !field.UseZero && field.Required {
 							if field.Zero != "" {
 								g.If(Add(code).Dot(field.FieldName).Op("==").Id(field.Zero)).Block(
-									Id("errs").Op("=").Append(Id("errs"), Qual("errors", "New").Call(Lit("env "+envName+" empty"))),
+									Id("errs").Op("=").Qual(multierrorPkg, "Append").Call(Id("errs"), Qual("errors", "New").Call(Lit("env "+envName+" empty"))),
 								)
 							}
 						}
 					})
 				if field.Required {
 					lookupIf.Else().Block(
-						Id("errs").Op("=").Append(Id("errs"), Qual("errors", "New").Call(Lit("env "+envName+" not set"))),
+						Id("errs").Op("=").Qual(multierrorPkg, "Append").Call(Id("errs"), Qual("errors", "New").Call(Lit("env "+envName+" not set"))),
 					)
 				}
 			})
